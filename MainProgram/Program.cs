2c289dc2217540b350afbeab7f63774bc65a5b54
hxl <1413698201@qq.com>
2024-07-01 13:34:52 +0800

    1

---
+++ MainProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.IO;
+
+namespace Excel2Json
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            JsonConvertHelper.ConfigureJsonInternal();
+
+            string json = File.ReadAllText("./ExcelToolConfig.json");
+
+            ExcelTool excelTool = new ExcelTool(json);
+
+            excelTool.ExportToJsonFile();
+
+            excelTool.ExportToCSFile();
+
+            Console.WriteLine("导出成功");
+
+            Console.ReadKey();
+
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Console.ReadKey();
+            //}
+        }
+    }
+}
